3d7a67f83b60682c31c9029f5c74049146b54feb
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2017-02-23 03:14:18 +0900

    modify files related to renaming to accounts

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PassesControllerTest < ActionDispatch::IntegrationTest
+class AccountTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
